751204fc85288d5c1fb647b7ac30b1e41c4cf590
Dhruv <Dhruv.nikuu53@gmail.com>
2024-10-02 18:49:24 -0400

    Initial commit

--- app.py
+++ app.py
@@ -1,18 +1,29 @@
-from flask import Flask, request, send_file
+from flask import Flask, request, send_file, jsonify, render_template
 import pyqrcode
-import os
+from PIL import Image
 
 app = Flask(__name__)
 
+# Route to serve the HTML form
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Defining an endpoint to generate the QR code
 @app.route('/generate_qr', methods=['POST'])
 def generate_qr():
-    link = request.json.get('link')
-    qrcode = pyqrcode.create(link)
-    qrcode_file = "qr_code.png"
-    qrcode.png(qrcode_file, scale=5)
+    try:
+        data = request.get_json()
+        link = data['link']
 
-    return send_file(qrcode_file, mimetype='image/png')
+        qrcode = pyqrcode.create(link)
+        qrcode_file = "qr_code.png"
+        qrcode.png(qrcode_file, scale=5)
+
+        return send_file(qrcode_file, mimetype='image/png')
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
